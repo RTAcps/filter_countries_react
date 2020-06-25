98a44d3fd77d88ae32bc8170ba788230666d532e
RODRIGO TEIXEIRA DE ANDRADE <62621434+RTAcps@users.noreply.github.com>
2020-06-25 20:11:47 -0300

    Updates

---
+++ precache-manifest.62b51f95859753a97a46996acd05ca0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ab4780d17155a5bf9e0cb298b7c31a",
+    "url": "/filter_countries_react/index.html"
+  },
+  {
+    "revision": "b50d4a16fb8a0ef0052f",
+    "url": "/filter_countries_react/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "623bfdbef4519e47701d",
+    "url": "/filter_countries_react/static/css/main.67cdc2a4.chunk.css"
+  },
+  {
+    "revision": "b50d4a16fb8a0ef0052f",
+    "url": "/filter_countries_react/static/js/2.d0345cff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/filter_countries_react/static/js/2.d0345cff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "623bfdbef4519e47701d",
+    "url": "/filter_countries_react/static/js/main.a34f1350.chunk.js"
+  },
+  {
+    "revision": "d13572b15338b95ad712",
+    "url": "/filter_countries_react/static/js/runtime-main.1e0cdade.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27c99fcef564bfff289e83bcdad114c5.js"
+  "/filter_countries_react/precache-manifest.62b51f95859753a97a46996acd05ca0e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/filter_countries_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
